44343d1192f5db98a162958a757f06f0b61fc3bf
bhushantbn <bhushantbn@gmail.com>
2024-02-06 21:31:45 +0530

    added clock project

---
+++ Clock/script.js
@@ -0,0 +1,19 @@
+const startButton=document.getElementById('start-button')
+
+const stopButton=document.getElementById('stop-button')
+let interval
+
+function showTime() {
+    const currentTime=new Date()
+    const time=`${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`
+    document.getElementById('time').innerText=time
+}
+function startTimer(){
+    clearInterval(interval)
+    interval=setInterval(showTime,1000)
+}
+function stopTimer(){
+    clearInterval(interval)
+}
+startButton.addEventListener('click',startTimer)
+stopButton.addEventListener('click',stopTimer)
